a8c70e7aeeafec5ecae0e2ff4f3f8ee4917f2f6e
ringo-117 <ykrina.117@gmail.com>
2026-08-21 15:14:32 +0900

    詳細モーダル実装

--- app/controllers/admin_stocks_controller.rb
+++ app/controllers/admin_stocks_controller.rb
@@ -2,13 +2,20 @@
 class AdminStocksController < ApplicationController
   def index
     @items = Item.kept.order(:item_code).page(params[:page]).per(20)
+
+    if turbo_frame_request?
+      render inline: ""
+    end
   end
 
   def show
     @item = Item.kept.find(params[:id])
 
-    if turbo_frame_request?
-      render partial: "item_detail_modal", locals: { item: @item }
+    unless turbo_frame_request?
+      redirect_to admin_stocks_path
+      return
     end
+
+    render partial: "item_detail_modal", locals: { item: @item }
   end
 end
